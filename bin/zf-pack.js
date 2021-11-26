f56d16b91e7c2645c6993b4eef999932ae6c412a
yaogengzhu <455947455@qq.com>
2021-11-26 13:21:07 +0800

    add

---
+++ bin/zf-pack.js
@@ -0,0 +1,12 @@
+#! /usr/bin/env node
+
+let path = require('path');
+
+// 拿到执行命令的路径
+let config = require(path.resolve('webpack.config.js'))
+// 拿到配置文件
+// console.log(config)
+let Complier = require('../lib/Compiler.js')
+let compiler = new Complier(config)
+// 编译时运行
+compiler.run()
